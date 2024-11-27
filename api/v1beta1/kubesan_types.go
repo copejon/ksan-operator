c5681a3917d829c12d0f9e711515e5d49801d24d
Jon Cope <jcope@redhat.com>
2024-11-26 19:38:22 -0600

    init apis

--- api/v1beta1/kubesan_types.go
+++ api/v1beta1/kubesan_types.go
@@ -23,13 +23,19 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+type VolumeGroup struct{}
+
+type Device struct{}
+
+type DeviceList []*Device
+
 // KubesanSpec defines the desired state of Kubesan
 type KubesanSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Kubesan. Edit kubesan_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	DeviceList  DeviceList  `json:"deviceList"`
+	VolumeGroup VolumeGroup `json:"volumeGroup"`
 }
 
 // KubesanStatus defines the observed state of Kubesan
